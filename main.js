c4e5e75410cecaf28879b430de98080bb722a1d0
Juli <julieta.anriquez91@gmail.com>
2022-09-01 23:09:30 -0300

    desafio javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+/**Ciclos /bucles */
+/**  */
+
+let respuesta= parseInt (prompt ("ingrese la cantidad de integrantes del grupo"));
+
+for(let i=0; i<=respuesta; i++)
+    console.log("Hola " + respuesta + " veces");
